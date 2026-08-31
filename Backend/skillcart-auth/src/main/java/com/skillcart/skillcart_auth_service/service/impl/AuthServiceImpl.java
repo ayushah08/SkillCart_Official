23ea4149cfdd09cae14cc12ef5cbae057a3288b7
ayushah08 <ayushah810@gmail.com>
2026-08-31 18:38:19 +0530

    changed the cors and security config to previous files were changed

--- Backend/skillcart-auth/src/main/java/com/skillcart/skillcart_auth_service/service/impl/AuthServiceImpl.java
+++ Backend/skillcart-auth/src/main/java/com/skillcart/skillcart_auth_service/service/impl/AuthServiceImpl.java
@@ -141,11 +141,12 @@ public class AuthServiceImpl implements AuthService {
             ResumeResponse resumeResponse =
                     restClient.get()
                             .uri(
-                                    "http://localhost:8082/api/v1/resume/user/{userId}",
+                                    "https://skillcart-resume.onrender.com/api/v1/resume/user/{userId}",
                                     userId
                             )
                             .retrieve()
                             .body(ResumeResponse.class);
+
 
 
             if (resumeResponse == null ||
